f3a94c7afde7354965c1cb160955b9aab0ac3636
Byter <bryanchacaliazasaavedra@gmail.com>
2023-06-09 00:58:04 -0500

    v1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BannerComponent,
+    CalculadoraComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent {
+  resG:string= ''
+  visor: string = ''
+  result:string = ''
+
+  escribeOp(c:string){
+    let cadena = this.visor + c
+    if(cadena.charAt(cadena.length - 2) == 'x' && cadena.charAt(cadena.length - 1) == 'x'){
+      this.visor += c
+      this.retroceder()
+    }else{
+      this.visor += c
+    }
+  }
+
+  escribeRes(){
+    let resultado:number = this.resolver(this.visor)
+    this.result = String(resultado)
+ }
+
+  resolver(op: string): number {
+    // Separa los números y operadores de la operación
+    let signos: string[] = op.match(/[+\-x/]/g) || []
+    let cadena:string[] = (op.split(/[+\-x/]/))
+    let numers = cadena.map(Number)
+    // Realiza la evaluación de la operación respetando el orden de operaciones
+    this.resOp(numers, signos, "x", "/")
+    this.resOp(numers, signos, "+", "-")
+
+    return numers[0]
+  }
+
+  resOp(numeros: number[], operadores: string[], op1: string, op2: string): void {
+    for (let i = operadores.length - 1; i >= 0; i--) {
+      let operador = operadores[i]
+      if (operador === op1 || operador === op2) {
+        let num1 = numeros[i]
+        let num2 = numeros[i + 1]
+        let resultado: number = 0
+        switch (operador) {
+          case "x":
+            resultado = num1 * num2;
+            break;
+          case "/":
+            resultado = num1 / num2;
+            break;
+          case "+":
+            resultado = num1 + num2;
+            break;
+          case "-":
+            resultado = num1 - num2;
+            break;
+        }
+        numeros.splice(i, 2, resultado);
+        operadores.splice(i, 1);
+      }
+    }
+  }
+
+  borrar(){
+    this.resG = this.result
+    this.visor = ''
+    this.result = ''
+  }
+  
+  retroceder(){
+    this.visor = this.visor.slice(0, -1)
+  }
+}
+
