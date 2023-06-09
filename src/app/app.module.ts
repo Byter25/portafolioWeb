83d9fda57c1374536b9d375155a54c709a0a4e9c
Byter <bryanchacaliazasaavedra@gmail.com>
2023-06-09 16:52:28 -0500

    PortafolioWeb_v1.2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { BannerComponent } from './banner/banner.component';
+import { SeccionComponent } from './seccion/seccion.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    BannerComponent,
+    SeccionComponent,
     CalculadoraComponent
   ],
   imports: [
