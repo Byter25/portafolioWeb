7d9bb1158ac5dc2a7efbd5a112f86674f9106439
Byter <bryanchacaliazasaavedra@gmail.com>
2023-06-09 21:44:20 -0500

    hola

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { SeccionComponent } from './seccion/seccion.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     SeccionComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
