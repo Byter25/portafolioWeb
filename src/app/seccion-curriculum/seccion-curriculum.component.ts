cf2acaa5c822337a70d855a8624d1d46ab743310
Byter <bryanchacaliazasaavedra@gmail.com>
2023-07-15 13:49:31 -0500

    SUBIENDO A PAGES

--- src/app/seccion-curriculum/seccion-curriculum.component.ts
+++ src/app/seccion-curriculum/seccion-curriculum.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-seccion-curriculum',
   templateUrl: './seccion-curriculum.component.html',
-  styleUrls: ['./seccion-curriculum.component.css']
+  styleUrls: ['./seccion-curriculum.component.css','../scroll-c/scroll-c.component.css']
 })
+
 export class SeccionCurriculumComponent {
 
 }
