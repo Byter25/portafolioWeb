58c93716313d1b46b689a949d2308c414ac4cbbf
Byter <bryanchacaliazasaavedra@gmail.com>
2023-07-15 23:20:36 -0500

    arreglao-v2

--- src/app/seccion-skills/seccion-skills.component.ts
+++ src/app/seccion-skills/seccion-skills.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Skills } from "../seccion.interface";
-import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -15,6 +14,7 @@ export class SeccionSkillsComponent {
     {id:2,skill:"javascript",progreso:"70%"},
     {id:3,skill:"python",progreso:"75%"},
     {id:4,skill:"java",progreso:"60%"},
+    {id:5,skill:"mainkera",progreso:"90%"}
   ]
 
   profesional:Skills[] =[
@@ -23,5 +23,27 @@ export class SeccionSkillsComponent {
     {id:3,skill:"python",progreso:"75%"},
     {id:4,skill:"java",progreso:"70%"},
   ]
+}
 
+export function elemento(){
+  const element = document.querySelector(".barra") as HTMLElement
+  const barras = document.querySelectorAll(".progreso")
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        barras.forEach((barra) => {
+          barra.classList.add("anima")
+        })
+        console.log("hola")
+      }else{
+        barras.forEach((barra) => {
+          barra.classList.remove("anima")
+        })
+        console.log("chao")
+      }
+    });
+  });
+  
+  // Inicia la observación del elemento objetivo
+  observer.observe(element);
 }
