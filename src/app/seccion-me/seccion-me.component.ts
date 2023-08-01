964cc90c4228d26c8d530a33dd2e7d2de961071a
Byter <bryanchacaliazasaavedra@gmail.com>
2023-08-01 00:55:28 -0500

    add-btn-agrandar

--- src/app/seccion-me/seccion-me.component.ts
+++ src/app/seccion-me/seccion-me.component.ts
@@ -6,6 +6,12 @@ import { Intereses } from "../seccion.interface";
   styleUrls: ['./seccion-me.component.css', '../app.component.css']
 })
 export class SeccionMeComponent {
+  agrandarCaja(){
+    const btnAgrandar = document.getElementById('btnAgrandar') as HTMLButtonElement
+    const texto = document.querySelector('.reziceCaja p') as HTMLParagraphElement
+    texto.style.maxHeight ="none"
+    btnAgrandar.style.display = "none"
+  }
   intereses:Intereses[] =[
     {id:1,interes:'node',icon:'fa-brands fa-node-js',color:'#68A063'},
     {id:2,interes:'sql',icon:'fa-solid fa-database',color:'#FCF443'},
