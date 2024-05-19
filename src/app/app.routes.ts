6111ad25d9bfbe10f6207f4e7ef657aeec13245e
Byter <bryanchacaliazasaavedra@gmail.com>
2024-05-18 21:30:24 -0500

    AAAhh?

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './articles/home/home.component';
-import { CalculadoraComponent } from './articles/proyectos/calculadora/calculadora.component';
-import { TaskListComponent } from './articles/proyectos/task-list/task-list.component';
 
 export const appRoutes: Routes = [
   {
