6113051261bc06bfea256b3d7e97d112731acbf1
Byter <bryanchacaliazasaavedra@gmail.com>
2024-09-05 12:33:52 -0500

    titulos-dinamicos

--- src/app/articles/articles.routes.ts
+++ src/app/articles/articles.routes.ts
@@ -5,19 +5,20 @@ import { ContactoComponent } from './contacto/seccion-contacto.component';
 import { CertificadosComponent } from './certificados.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { Title } from '@angular/platform-browser';
 
 export const ARTICLES_ROUTES: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'services', component: ServiciosComponent },
+  { path: 'services', component: ServiciosComponent,data: {title: 'Byter-Servicios'}},
   {
     path: 'proyects',
     component: ProyectosComponent,
     loadChildren: () =>
-      import('./proyectos/proyectos.routes').then((n) => n.PROYECTOS_ROUTES),
+      import('./proyectos/proyectos.routes').then((n) => n.PROYECTOS_ROUTES),data: {title: 'ByterDev-Proyectos'}
   },
-  { path: 'certificates', component: CertificadosComponent },
-  { path: 'contact', component: ContactoComponent },
-  { path: 'about', component: SobreMiComponent},
-  { path: 'privacity', component: HomeComponent },
-  { path: 'cookies', component: HomeComponent },
+  { path: 'certificates', component: CertificadosComponent, data: {title: 'ByterDev-Certifiacdos'}},
+  { path: 'contact', component: ContactoComponent, data: {title: 'Byter-Contacto'}},
+  { path: 'about', component: SobreMiComponent, data: {title: 'Byter-AcercaDe'}},
+  { path: 'privacity', component: HomeComponent, data: {title: 'ByterDev'} },
+  { path: 'cookies', component: HomeComponent, data: {title: 'ByterDev'} },
 ];
