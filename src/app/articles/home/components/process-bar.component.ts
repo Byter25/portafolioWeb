ca5c222c947bcb6c7b64bbf29ed32de14cff71dd
Bryan Alexander <117506385+Byter25@users.noreply.github.com>
2024-09-15 05:32:37 +0000

    color-fondo-progreso

--- src/app/articles/home/components/process-bar.component.ts
+++ src/app/articles/home/components/process-bar.component.ts
@@ -33,7 +33,7 @@ import { FloatComponent } from '@shared/components/float.component';
 }
 
 progress::-webkit-progress-bar {
-  @apply rounded-lg bg-gray-100/10
+  @apply rounded-lg bg-gray-100/50
 }
 
 progress::-webkit-progress-value {
