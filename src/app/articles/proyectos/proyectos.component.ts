aabdf5f4901a6a86cddbd85f0313a53afe06a12f
Byter <bryanchacaliazasaavedra@gmail.com>
2024-10-29 13:25:52 -0500

    estructurando

--- src/app/articles/proyectos/proyectos.component.ts
+++ src/app/articles/proyectos/proyectos.component.ts
@@ -41,51 +41,54 @@ export class ProyectosComponent implements OnInit {
       {
         id: 1,
         name: 'Tailwind CSS',
-        icon: 'assets/svg/tailwind-css.svg',
-        LenguajesId: [1, 2], // IDs de los lenguajes
+        icon: 'assets/svg/frameworks/tailwind-css.svg',
+        LenguajesId: [2], // IDs de los lenguajes
       },
       {
         id: 2,
         name: 'Bootstrap',
-        icon: 'assets/svg/bootstrap.svg',
+        icon: 'assets/svg/frameworks/bootstrap.svg',
         LenguajesId: [1, 2, 3],
       },
       {
         id: 3,
         name: 'Spring Boot',
-        icon: 'assets/svg/spring-boot.svg',
-        LenguajesId: [4, 5],
+        icon: 'assets/svg/frameworks/spring-boot.svg',
+        LenguajesId: [4],
       },
       {
         id: 4,
         name: '.NET',
-        icon: 'assets/svg/net.svg',
-        LenguajesId: [6, 7],
+        icon: 'assets/svg/frameworks/net.svg',
+        LenguajesId: [5],
       },
       {
         id: 5,
         name: 'Angular',
-        icon: 'assets/svg/angular.svg',
-        LenguajesId: [8, 3],
+        icon: 'assets/svg/frameworks/angular.svg',
+        LenguajesId: [1,2,6],
       },
       {
         id: 6,
         name: 'Astro',
-        icon: 'assets/svg/astro.svg',
+        icon: 'assets/svg/frameworks/astro.svg',
         LenguajesId: [3, 9],
+      },
+      {
+        id: 7,
+        name: 'Express',
+        icon: 'assets/svg/frameworks/express-js.svg',
+        LenguajesId: [1,2,3],
       },
     ];
 
     this.lenguajes = [
-      { id: 1, name: 'HTML', icon: 'assets/svg/html.svg' },
-      { id: 2, name: 'CSS', icon: 'assets/svg/css.svg' },
-      { id: 3, name: 'JavaScript', icon: 'assets/svg/javascript.svg' },
-      { id: 4, name: 'Java', icon: 'assets/svg/java.svg' },
-      { id: 5, name: 'Kotlin', icon: 'assets/svg/kotlin.svg' },
-      { id: 6, name: 'C#', icon: 'assets/svg/csharp.svg' },
-      { id: 7, name: 'VB.NET', icon: 'assets/svg/vbnet.svg' },
-      { id: 8, name: 'TypeScript', icon: 'assets/svg/typescript.svg' },
-      { id: 9, name: 'Markdown', icon: 'assets/svg/markdown.svg' },
+      { id: 1, name: 'HTML', icon: 'assets/svg/lenguajes/html.svg' },
+      { id: 2, name: 'CSS', icon: 'assets/svg/lenguajes/css.svg' },
+      { id: 3, name: 'JavaScript', icon: 'assets/svg/lenguajes/javascript.svg' },
+      { id: 4, name: 'Java', icon: 'assets/svg/lenguajes/java.svg' },
+      { id: 5, name: 'C#', icon: 'assets/svg/lenguajes/csharp.svg' },
+      { id: 6, name: 'TypeScript', icon: 'assets/svg/lenguajes/typescript.svg' },
     ];
 
     this.proyectos = [
