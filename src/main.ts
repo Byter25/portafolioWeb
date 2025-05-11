cb9438e9b2b56cc378e5e442a0be7822094164af
bryan saavedra <bryanchacaliazasaavedra@gmail.com>
2025-05-10 21:07:40 -0500

    meta-google-correct

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,6 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  let METAtitulo:string = "ByterDev | Fullstack Developer";
+  let METAdescripcion:string = "Fullstack Developer proficient in Angular, .NET, and databases. Contact me to learn more about my projects.";
