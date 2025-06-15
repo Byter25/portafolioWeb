f3f250cc8a67579aed5838efa31594cd156f2438
byterdev <bryanchacaliazasaavedra@gmail.com>
2025-06-15 04:33:46 -0500

    fixed--bg-menu-right

--- src/app/shared/components/navbar/components/menu-right.component.ts
+++ src/app/shared/components/navbar/components/menu-right.component.ts
@@ -22,7 +22,7 @@ import { TranslocoModule } from '@ngneat/transloco';
     </label>
     <ul
       id="rightnav"
-      class=" -left-full fixed md:hidden bg-inherit top-0 w-2/3 h-screen uppercase transition-all duration-300 ease-in-out font-bold"
+      class=" -left-full fixed md:hidden bg-black top-0 w-2/3 h-screen uppercase transition-all duration-300 ease-in-out font-bold"
     >
       <label for="navright" class="text-center flex items-center px-4 py-2">
         <mat-icon>close</mat-icon>
